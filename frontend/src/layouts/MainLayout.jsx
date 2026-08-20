b232d0e55851fbd9b293c1daf28d6a3ac165d2e0
unknown <edwardvaron001@gmail.com>
2026-08-20 15:54:52 -0500

    Se corrigio el error del doble slider y se ajusto el alto de la pagina para evitar zonas vacias

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -2,9 +2,9 @@ import Sidebar from "../components/Sidebar";
 
 export default function MainLayout({ children }) {
     return (
-        <div className="flex h-screen bg-gray-100 dark:bg-gray-900 dark:text-white transition-colors duration-200">
+        <div className="flex bg-gray-100 dark:bg-gray-900 dark:text-white transition-colors duration-200">
             <Sidebar />
-            <div className="flex-1 p-8 overflow-auto">
+            <div className="flex-1 p-8 overflow-hidden">
                 {children}
             </div>
         </div>
